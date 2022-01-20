722bcdf4da268c93991fd39711b6a09ee1ac6e31
Ryan Meredith <ryan48meredith@yahoo.com>
2022-01-20 09:37:48 -0600

    Updated Code

--- Multivariant Regression Model.py
+++ Multivariant Regression Model.py
@@ -11,30 +11,33 @@ import matplotlib.pyplot as plt
 ##import plotly.express as px
 ##import hvplot.pandas
 from sklearn.linear_model import LinearRegression as lr
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
 # Import database from CSV
 toc_db = pd.read_csv('Multivariant_Regression_TOC.csv')
 
-# Pull out the dependent variables from the "Redox and Production Proxies" to use in model (i.e. 'Mo','U','Ni','Cu','Cr','Zn','Pb','Th','V','Ba','S','Fe')
+# Pull out the dependent variables from the "Redox and Production Proxies" to use in model (i.e. 'Mo','U','Ni','Cu','Cr','Zn','Pb','Th','V','Ba','Se','S','Fe')
 # Remove elements that are not present in dataset (i.e. if sum of element equals zero)
-x = pd.DataFrame(toc_db[['Mo','U','Ni','Cu','Cr','Zn','Pb','Th','V','Ba','S','Fe']])
-##x_name_df = pd.DataFrame(x.columns).T
+x = pd.DataFrame(toc_db[['Mo','U','Ni','Cu','Cr','Zn','Pb','Th','V','Ba','Se','S','Fe']])
+# Replace nan with zero's
+x = x.fillna(0.0001)
 
 # Pull out the independent variable "TOC"
 y = pd.DataFrame(toc_db[['TOC']])
 
 # Remove features that may have variance that are orders of magnitude larger than others. 
+# StandardScaler sets whole data set to Zero (need to investigate if each element is 0 to 1)
 ##x_scaled = StandardScaler().fit_transform(x)
-x_scaled2 = MinMaxScaler().fit_transform(x)
+x_scaled = MinMaxScaler().fit_transform(x)
 
 # Split data into train and test
-x_train, x_test, y_train, y_test = train_test_split(x_scaled2, y, test_size = .25, random_state = 42)
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size = .15, random_state = 42)
 
-# Apply Linear Regression Model to split data
+# Apply Linear Regression Model to split data (Intercept is set to Zero)
 model = lr().fit(x_train, y_train)
+##model = lr(fit_intercept=False).fit(x_train, y_train)
 
 # Getting "predict" from LR Model
 y_predict = model.predict(x_test)
@@ -45,19 +48,20 @@ y_predict_df = pd.DataFrame(y_predict)
 # Finding R-squared of LR Model
 score = r2_score(y_test,y_predict)
 
-# Finding Coefficient & Y-Intercept of LR Model
+# Finding Coefficient & Y-Intercept of LR Model (Un-Null 'intercept' code if LR model has intercept)
 coef = model.coef_
-coef_df = pd.DataFrame(coef)
-##coef_df = coef_df.assign(column_name = x_name_df)
+coef_df = pd.DataFrame(coef, columns=x.columns)
 intercept = model.intercept_
 intercept_df = pd.DataFrame(intercept)
+intercept_df.columns = ['Intercept']
 
 # Merging coef and intercept for export
-C_I = np.concatenate([coef_df,intercept_df], axis=None)
+C_I = pd.concat([coef_df,intercept_df], axis=1).T
+
 
 # Write Coefficients & Y-intercept to Excel
-df = pd.DataFrame(C_I,).T
-##df2 = df.assign(column_name = x_name_df)
+df = pd.DataFrame(C_I).T
+##df = pd.DataFrame(coef_df)
 df.to_excel(excel_writer = "TOC_Coefficient.xlsx", index=False)
 
 # Scatter Plot of Prediction vs TOC
@@ -66,8 +70,8 @@ plt.scatter(x=y_predict_df,y=y_test,c=y_predict_df,marker="d",cmap="jet", edgeco
 plt.title("TOC vs Predicted TOC")
 plt.xlabel("Predicted TOC")
 plt.ylabel("TOC (wt%)")
-plt.legend([f'Rsq ={score: .2f}'],loc="lower right",shadow=True,frameon=True,fancybox=True)
-plt.colorbar()
+plt.legend([f'Rsq ={score: .2f}'],loc="best",shadow=True,frameon=True,fancybox=True)
+##plt.colorbar()
 
 # Plot a trendline
 m, b = np.polyfit(y_predict_df[0], y_test, 1)
